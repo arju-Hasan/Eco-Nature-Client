13b6b3c595aac4675ad497a1cf562407567bf50d
Arju-Hasan <rajuahmeed698@gmail.com>
2025-11-11 20:44:34 +0600

    add firebase auth config env.local

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,23 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-// 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey:import.meta.env.VITE_apiKey,
-//   authDomain:import.meta.env.VITE_authDomain,
-//   projectId:import.meta.env.VITE_projectId,
-//   storageBucket:import.meta.env.VITE_storageBucket,
-//   messagingSenderId:import.meta.env.VITE_messagingSenderId,
-//   appId:import.meta.env.VITE_appId,
-// };
-// 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export default app;
-
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -25,14 +5,34 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC1UXQLgcCqyNPTTuvR8aIs4Mr9kVUNWRw",
-  authDomain: "eco-nature-client.firebaseapp.com",
-  projectId: "eco-nature-client",
-  storageBucket: "eco-nature-client.firebasestorage.app",
-  messagingSenderId: "907821229285",
-  appId: "1:907821229285:web:46cf663130e7642b46200c"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+// 
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC1UXQLgcCqyNPTTuvR8aIs4Mr9kVUNWRw",
+//   authDomain: "eco-nature-client.firebaseapp.com",
+//   projectId: "eco-nature-client",
+//   storageBucket: "eco-nature-client.firebasestorage.app",
+//   messagingSenderId: "907821229285",
+//   appId: "1:907821229285:web:46cf663130e7642b46200c"
+// };
+// 
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export default app;
